3d3e7f24675591753a8bf2bde9c654c24aa19647
Philipp Matthes <philippmatthes@p200300c27bc020000064a5e58fa92fc3.dip0.t-ipconnect.de>
2017-08-08 09:14:23 +0200

    Initial Commit

--- DragTimer/ViewController.swift
+++ DragTimer/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Speed label on main view for speed displaying
+    @IBOutlet weak var speedReplacementLabel: UILabel!
+    @IBOutlet var background: UIView!
+    @IBOutlet weak var maxSpeedLabel: UILabel!
+    
+    
+    let manager = CLLocationManager()
+    var speedLog = [Double]()
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        self.speedLog.append(location.speed)
+        let convertedSpeed = Double(round(1000 * location.speed * 3.6)/1000)
+        
+        DispatchQueue.main.async {
+            self.speedReplacementLabel.text = "\(convertedSpeed) km/h"
+        }
+        
+        let maxSpeed = self.speedLog.max()!
+        let convertedMaxSpeed = Double(round(1000 * maxSpeed * 3.6)/1000)
+        self.maxSpeedLabel.text = "Max Speed: \(convertedMaxSpeed) km/h"
+        
+        
+        //background.backgroundColor = UIColor(red: (255.0/255.0), green: (255.0/255.0), blue: (255.0/255.0), alpha: CGFloat(location.speed/maxSpeed))
+        
+        
+ 
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
